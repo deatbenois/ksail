3c53d8db0e3d227683f515f2f2be173836477e16
Dean Benois <deatbenois@users.noreply.github.com>
2026-04-25 02:13:53 +0800

    Add personal note about home lab usage in main comment

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 // ksail is a Kubernetes cluster management tool that simplifies
 // the lifecycle of local and remote Kubernetes clusters using
 // GitOps principles with Flux and k3d.
+//
+// Personal fork: using this for home lab cluster management.
 func main() {
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
